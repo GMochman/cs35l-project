bada9bf1debb98ffa4c3d8ff6c65f2b52e83803c
cassiestephen <cassie.v.stephen@gmail.com>
2023-03-10 20:41:49 -0800

    Search Button Fixed Search button fixed to no longer reload the webpage when clicked.

--- src/components/search.js
+++ src/components/search.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 
-// create a component
+// create a component 
 const Form = ({placeHolder, isSearching}) => {
     return(
         <form>
             <div className="search-input">
                 <input type="text" placeholder={placeHolder} onChange={isSearching}/>
-                <button className="search-locations" type="submit"> 
+                <button type="button"> 
                     <i class="fa-solid fa-magnifying-glass"></i> 
                 </button> 
             </div>
