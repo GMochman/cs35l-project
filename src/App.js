0db24926c32d617449248250305c1bc05863b692
Kimberly Yu <kimberlyyu@ucla.edu>
2023-03-11 23:38:07 -0800

    Resolved conflicts, added Emily's css styling, my like/unlike button, and Emily and I's reviews

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
 import Home from './pages/Home';
 import CreatePost from './pages/create-post/CreatePost';
 import Login from './pages/Login';
 import { useState } from 'react';
 import { signOut } from 'firebase/auth';
-=======
-import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost";
-import Login from "./pages/Login";
-import { useState } from "react";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
->>>>>>> origin
 import { auth } from "./firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -36,7 +28,6 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
   <Router>
     <nav>
       <Link to="/"> Home </Link>
@@ -64,34 +55,6 @@ function App() {
       <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
     </Routes>
   </Router>
-=======
-    <Router>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/createpost"> Add Review </Link>
-
-        {isAuth ? (
-          <div className="user">
-            <div>{user?.displayName}</div>
-            <img src={user?.photoURL || ""} height="30px" width="30px" alt="" />
-          </div>
-        ) : (
-          <div></div>
-        )}
-
-        {!isAuth ? (
-          <Link to="/login"> Login </Link>
-        ) : (
-          <Link onClick={signUserOut}> Log Out </Link>
-        )}
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost user={user} />} />
-        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-      </Routes>
-    </Router>
->>>>>>> origin
   );
 }
 
