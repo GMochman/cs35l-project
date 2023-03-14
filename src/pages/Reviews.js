d7e423896bfa375ee2d46f192bd35fe3e8230619
cassiestephen <cassie.v.stephen@gmail.com>
2023-03-13 17:35:57 -0700

    Search Bar Implemented Inside Reviews Page
    addLocation.js changed to be search.js,
    representing search bar
    plus icon in Form function changed to magnifying glass
    search bar from feature/login branch
    implemented in review.js

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -3,10 +3,12 @@ import { db } from "../firebase-config";
 import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom"
 import { Review } from './Review'
+import Form from '../components/search';
 
 export const Reviews = () => {
   const [reviewsList, setReviewsList] = useState(null);
   const reviewsRef = collection(db, "reviews");
+  const [searchKeyword, setSearchKeyword] = useState(""); 
 
   const getReviews = async () => {
     const data = await getDocs(reviewsRef);
@@ -20,19 +22,28 @@ export const Reviews = () => {
   }, []);
 
   return (
-    <div>
-      {reviewsList?.map((review) => (
       <div>
-        <Link to={review.id}>
-            <div className="review-snippets">
+        <div className="search-bar">
+          <Form placeHolder={"Find a Location"} isSearching={(event) => 
+          {setSearchKeyword(event.target.value)}}/> 
+         </div>
+        {reviewsList?.filter((review)=> {
+        if (searchKeyword == "") {
+          return review;
+        } else if (review.title.toLowerCase().includes(searchKeyword.toLowerCase())) {
+          return review;
+}}).map((review) => (
+          <div>
+            <Link to={review.id}>
+              <div className="review-snippets">
                 {review.title}
-            </div>
-        </Link>
-        {/* {reviewsList?.map((review) => ( */}
-        {/* <Review review={review}/> */}
-        {/* ))} */}
+                </div>
+          </Link>
+          {/* {reviewsList?.map((review) => ( */}
+          {/* <Review review={review}/> */}
+          {/* ))} */}
+       </div>
+        ))}
       </div>
-      ))}
-    </div>
   )
 }
